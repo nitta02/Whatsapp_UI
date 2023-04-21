4aa90f1656fb39c9c6220e05fb600729d655d719
nittoranjansarkertanmoy02 <nittaranjansarkertanmoy2022@gmail.com>
2023-04-21 10:19:13 +0600

    day2

---
+++ lib/constants/subtitle_constants.dart
@@ -0,0 +1,19 @@
+const subTitle1 = 'Hello, Welcome';
+const subTitle2 = 'Hello, Welcome';
+const subTitle3 = 'Hello, Welcome';
+const subTitle4 = 'Hello, Welcome';
+const subTitle5 = 'Hello, Welcome';
+const subTitle6 = 'Hello, Welcome';
+const subTitle7 = 'Hello, Welcome';
+const subTitle8 = 'Hello, Welcome';
+
+const subTiltes = [
+  subTitle1,
+  subTitle2,
+  subTitle3,
+  subTitle4,
+  subTitle5,
+  subTitle6,
+  subTitle7,
+  subTitle8,
+];
